9e8019743b432b2b21872ae716c8ed3981c53b61
Flawn <fl4wn@protonmail.com>
2025-08-23 20:35:36 -0600

    chore: 🔧 Remove unneded files

--- app/welcome/welcome.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import { Welcome } from "./welcome";
-
-test("should render", () => {
-  render(<Welcome />);
-
-  expect(screen.getByText("React Router Docs")).toBeInTheDocument();
-});
