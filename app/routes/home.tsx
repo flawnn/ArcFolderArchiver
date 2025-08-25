e2219955d8e111a258cb4ecdee5ec789513f93d2
Flawn <fl4wn@protonmail.com>
2025-08-24 23:47:35 -0600

    refactor(app): ♻️ Minor changes to accomodate for older changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Arc Folder Archiver" },
+    { name: "description", content: "Flatten your Arc folders" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return "Hi";
 }
--- app/server.ts
+++ app/server.ts
@@ -1,3 +1,15 @@
+import { Hono } from "hono";
 import { createHonoServer } from "react-router-hono-server/bun";
+import { archiveController } from "./api/archive/archive.controller";
 
-export default await createHonoServer();
+// Create a root Hono app
+const app = new Hono();
+
+// Archive API Endpoint
+app
+  .post("/api/archive", archiveController.postFolder)
+  .delete("/api/archive", archiveController.deleteFolder);
+
+export default await createHonoServer({
+  app,
+});
