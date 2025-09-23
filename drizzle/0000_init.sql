c0c39aacb93a07e98afab853637ade1132b2fa26
Flawn <fl4wn@protonmail.com>
2025-09-23 20:39:21 +0200

    chore(db): 🔧 Fix Drizzle Migration mess

--- drizzle/0000_init.sql
+++ drizzle/0000_init.sql
@@ -5,10 +5,10 @@ CREATE TABLE "archived_folders" (
 	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
 	"arc_id" text NOT NULL,
 	"folder_data" jsonb NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
 	"last_fetched_at" timestamp DEFAULT now() NOT NULL,
 	"delete_at" timestamp,
 	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp,
-	CONSTRAINT "archived_folders_arcId_unique" UNIQUE("arc_id")
+	CONSTRAINT "archived_folders_arc_id_unique" UNIQUE("arc_id")
 );--> statement-breakpoint
